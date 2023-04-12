73ede283f2532580df0ee1abcb4c47d8c4f39c97
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2023-04-12 06:46:43 -0700

    printing sum of two command line raguments

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 if (process.argv.length === 3 && +process.argv[2]) {
   for (let i = 0; i < process.argv[2]; i++) {
     for (let j = 0; j < process.argv[2]; j++) {
-      process.stdout.write('x');
+      console.log('x'.replace(/(\r\n|\n|\r)/gm, ''))
     }
     console.log('');
   }
