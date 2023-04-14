4e21e531cae19d934f0a07c5e9ae423de5ebfc34
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2023-04-14 13:53:18 -0700

    file system module example

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const file_system = require('fs');
-const a = file_system.readFileSync(process.argv[2], 'utf8');
-const b = file_system.readFileSync(process.argv[3], 'utf8');
-file_system.writeFileSync(process.argv[4], a + b);
+const fileSystem = require('fs');
+const a = fileSystem.readFileSync(process.argv[2], 'utf8');
+const b = fileSystem.readFileSync(process.argv[3], 'utf8');
+fileSystem.writeFileSync(process.argv[4], a + b);
