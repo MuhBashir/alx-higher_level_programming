081dcb152509d772c8d57f3658ab350c32f3122e
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2023-04-14 03:52:44 -0700

    returning empty object under some certain condition

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return  'Rctangle' + {};
+      return 'Rctangle' + {};
     }
     this.width = w;
     this.height = h;
