efcd47a1f2f8b513d67c2bb7cef3d3d617f94b41
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2023-04-20 07:07:29 -0700

    adding relationship between city and state

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,22 @@
+"""
+This script defines a City class
+to work with MySQLAlchemy ORM and States.
+"""
+
+from relationship_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """City class
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id : The id of the class
+        name: The name of the class
+        state_id: The state the city belongs to
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
