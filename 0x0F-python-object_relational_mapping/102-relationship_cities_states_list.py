030aa4da1b5bf2ebda41bc6c3b44720da996607f
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2023-04-20 08:01:33 -0700

    list all city objects

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -2,7 +2,7 @@
 """
 This script lists all City objects from the database hbtn_0e_101_usa
 """
-from sys import agrv
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import create_engine
