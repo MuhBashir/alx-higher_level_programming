5960025a5632d11ae89cff8d81707da79037ac43
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2023-04-27 14:11:43 -0700

    correction

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script send a post request and display response of the body.
+#curl sends POST req to URL, display response body
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
-
